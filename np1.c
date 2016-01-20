1590aa9f26f978374729f91674ed7f5d0364ecd7
acemaster <vivekhtc25@gmail.com>
2016-01-20 13:35:58 +0530

    Complete 2/3 of programs

--- np1.c
+++ np1.c
@@ -1,18 +1,51 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
 
-int main()
+
+int main(int argc,char const *argv[])
 {
-	char buf[]="Hello";
-	int fd1=open("input.txt",O_RDWR);
-	int fd2=open("output.txt",O_RDWR);
-	printf("%d......%d\n",fd1,fd2);
-	int a=dup2(fd1,fd2);
-	printf("%d\n",a);
-	if(write(fd1,&buf,4) == -1)
-		printf("Error\n");
-	close(fd1);
-	close(fd2);
+	int n;
+	sscanf(argv[1], "%d", &n);
+	int a;
+	char buf[10];
+	while(n >0){
+		int pid=fork();
+		if(pid == 0){
+			int fd1=open("input.txt",O_RDWR);
+			int fd2=open("output.txt",O_RDWR);
+			if(n%2 != 0)
+			{
+				a=dup2(fd2,1);
+				if(read(fd1,&buf,5) == -1)
+				{
+					printf("Read error at phase %d\n",n);
+				}
+				printf("%s",buf);
+
+			}
+			else
+			{
+				a=dup2(fd1,1);
+				if(read(fd2,&buf,5) == -1)
+				{
+					printf("Read error at phase %d\n",n);
+				}
+				printf("%s",buf);
+
+			}
+			close(fd1);
+			close(fd2);
+			exit(0); 
+		}
+		else if(pid > 0){
+			int returnStatus; 
+			waitpid(pid,&returnStatus, 0);
+			printf("Process %d done\n",n);
+			n--;
+		}
+    }
 	return 0;
 }
 
--- shm.c
+++ shm.c
@@ -6,41 +6,32 @@
 
 static void sig_usr(int signo);
 
+
 int main(){
 	key_t key1,key2;
 	int shmflg;
 	int shmid1,shmid2;
 	int size;
-	char *shm,*shm2,*s;
+	int shm,shm2,s;
 	key1=100;
 	key2=200;
 	size=10000;
 	if ((shmid1 = shmget (key1, size, IPC_CREAT | 0666)) == -1) {
 	   perror("shmget: shmget failed"); exit(1); } else {
 	   (void) fprintf(stderr, "shmget: shmget returned %d\n", shmid1);
-	   exit(0);
 	  }
 	if ((shmid2 = shmget (key2, size, IPC_CREAT | 0666)) == -1) {
 	   perror("shmget: shmget failed"); exit(1); } else {
 	   (void) fprintf(stderr, "shmget: shmget returned %d\n", shmid2);
-	   exit(0);
 	  }
-	if ((shm = shmat(shmid1, NULL, 0)) == (char *) -1) {
-        perror("shmat");
-        exit(1);
-    }
+	shm = (int)shmat(shmid1, NULL, 0);
     int flag=0;
-    for(*s=shm;s!=NULL;s++)
-    {
-    	flag=1;
-    }
-    if(flag == 0)
-    	*shm='a';
-    if ((shm2 = shmat(shmid2, NULL, 0)) == (char *) -1) {
-        perror("shmat");
-        exit(1);
-    }
-    *shm2=*shm+1;
+    if(shm<0)
+    	shm=0;
+    printf("Value recieved y: %d\n",shm);
+    shm2 = (int)shmat(shmid2, NULL, 0);
+    shm2=shm+1;
+    printf("Value sent x: %d\n",shm2);
     signal(SIGUSR1,sig_usr);
     raise(SIGUSR1);
 }
