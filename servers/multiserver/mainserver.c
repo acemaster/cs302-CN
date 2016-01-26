6949a48ead57978de777708ade4aad60da7d0b25
acemaster <vivekhtc25@gmail.com>
2016-01-26 10:19:07 +0530

    Multiserver almost complete

--- servers/multiserver/mainserver.c
+++ servers/multiserver/mainserver.c
@@ -5,17 +5,32 @@ struct logfds{
 	int number;
 	int fd[10];
 };
+
+int n_s1;
+int n_s2;
 //Log Server
 int main(int argc,char *argv[]){
-	printf("\nLog Server is starting................");
+	printf("\nMain Server is starting................");
 	int fd;
 	struct logfds logs;
+	struct logfds logwrite;
 	int i;
 	logs.number=atoi(argv[1]);
+	logwrite.number=logs.number;
+	n_s1=atoi(argv[2]);
+	n_s2=atoi(argv[3]);
+	printf("Max clients: %d, S1: %d, S2: %d\n",logs.number,n_s1,n_s2);
 	for(i=0;i<logs.number;i++){
 		char buf[50];
-		snprintf(buf, sizeof(buf), "%s%d", "logfifo", i);
+		char buf2[50];
+		snprintf(buf, sizeof(buf), "%s%d", "mainfifo", i);
 		if(mkfifo(buf,0777) !=0)
+		{
+			perror ("The following error occurred");
+			exit(0);
+		}
+		snprintf(buf2, sizeof(buf2), "%s%d", "mainfifowrite", i);
+		if(mkfifo(buf2,0777) !=0)
 		{
 			perror ("The following error occurred");
 			exit(0);
@@ -25,6 +40,32 @@ int main(int argc,char *argv[]){
 			perror ("The following error occurred");
 			exit(0);
 		}
+		if((logwrite.fd[i]=open(buf2,O_RDWR)) < 0)
+		{
+			perror ("The following error occurred");
+			exit(0);
+		}
+	}
+
+	if(mkfifo("S1fifo",0777) !=0)
+		{
+			perror ("The following error occurred");
+			exit(0);
+	}
+	if(mkfifo("S2fifo",0777) !=0)
+		{
+			perror ("The following error occurred");
+			exit(0);
+	}
+	if(mkfifo("S1fifowrite",0777) !=0)
+		{
+			perror ("The following error occurred");
+			exit(0);
+	}
+	if(mkfifo("S2fifowrite",0777) !=0)
+		{
+			perror ("The following error occurred");
+			exit(0);
 	}
 	//Shared memory creation and putting log fd info
 	int shmid1;
@@ -64,6 +105,21 @@ int main(int argc,char *argv[]){
 					if(read(fds[j].fd,buf,100) > 0)
 					{
 						printf("%s\n",buf);
+						if(strcasecmp(buf,"s1\n") == 0 && n_s1 > 0)
+						{
+							n_s1--;
+							printf("S1 allocated\n");
+							write(logwrite.fd[j],"S1fifo",35);
+						}
+						else if(strcasecmp(buf,"s2\n") == 0 && n_s2 > 0)
+						{
+							n_s2--;
+							printf("S2 allocated\n");
+							write(logwrite.fd[j],"S2fifo",35);
+						}
+						else{
+							printf("No servers avaliable\n");
+						}
 					}
 					fflush(stdout);
 				}
