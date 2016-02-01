2ab7dd78a28f0c14bdd910418f5f6595f92d67a6
acemaster <vivekhtc25@gmail.com>
2016-02-01 08:22:34 +0530

    Completed all in one

--- servers/allinoneserver/README.md
+++ servers/allinoneserver/README.md
@@ -2,10 +2,9 @@
 
 Steps:
 
-1. Compile the mainserver.c,server.c,client.c ``` gcc mainserver.c -o mainserver; gcc server.c -lpthread -o server; gcc client.c -lpthread -o clientprog```
-2. Depending on the number of servers connected to the mainserver start logserver with that number ```./mainserver 2 1 1 ``` Shared memory is used to automatically create the number of logfifos. 2 for max clients, 1 for no. of s1 servers and the next 1 for the number of s2 servers (currently supported for 1 s1 or s2 -> 1 client model)
-3. Start client as ```./clientprog ``` 
-4. Start server as the serve fifo name. ``` ./server S1fifo ```
-5. Chat away
+1. Compile the mainserver.c,server.c,client.c ``` gcc mainserver.c -o mainserver```
+2. Comipile and Start client as ```gcc client.c -o clientprog; ./clientprog ``` 
+3. Start server as the serve fifo name. ``` ./server```
+4. This is a practice server utilizing different methods of communication
 
-![log server](multiserver.png)
+![log server](allinone.png)
--- servers/allinoneserver/mainserver.c
+++ servers/allinoneserver/mainserver.c
@@ -1,131 +1,104 @@
 #include "vivek.h"
 #include <poll.h>
 
+static void sig_usr(int signo);
+
 struct logfds{
 	int number;
 	int fd[10];
+	int pipefd[2];
+	int totalnumber;
 };
 
-int n_s1;
-int n_s2;
-//Log Server
+
+/*
+	The clients are as follows:
+		1. Keyboard :Done
+		2. Pipe fd :Done
+		3. Fifo fd: Done
+		4. Popen
+		5. Signal : Done
+*/
+
+//All in one Server
 int main(int argc,char *argv[]){
 	printf("\nMain Server is starting................");
 	int fd;
 	struct logfds logs;
 	struct logfds logwrite;
 	int i;
-	logs.number=atoi(argv[1]);
-	logwrite.number=logs.number;
-	n_s1=atoi(argv[2]);
-	n_s2=atoi(argv[3]);
-	printf("Max clients: %d, S1: %d, S2: %d\n",logs.number,n_s1,n_s2);
-	for(i=0;i<logs.number;i++){
-		char buf[50];
-		char buf2[50];
-		snprintf(buf, sizeof(buf), "%s%d", "mainfifo", i);
-		if(mkfifo(buf,0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-		}
-		snprintf(buf2, sizeof(buf2), "%s%d", "mainfifowrite", i);
-		if(mkfifo(buf2,0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-		}
-		if((logs.fd[i]=open(buf,O_RDWR)) < 0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-		}
-		if((logwrite.fd[i]=open(buf2,O_RDWR)) < 0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-		}
+	dup2(logs.fd[0],0);
+	if((logs.fd[1]=open("mainfifo",O_RDWR)) < 0)
+	{
+		perror ("The following error occurred");
+		exit(0);
 	}
-
-	if(mkfifo("S1fifo",0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
+	if(mkfifo("mainfifo2",0777) !=0)
+	{
+		perror ("The following error occurred");
+		exit(0);
 	}
-	if(mkfifo("S2fifo",0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
+	if((logs.fd[2]=open("mainfifo2",O_RDWR)) < 0)
+	{
+		perror ("The following error occurred");
+		exit(0);
 	}
-	if(mkfifo("S1fifowrite",0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-	}
-	if(mkfifo("S2fifowrite",0777) !=0)
-		{
-			perror ("The following error occurred");
-			exit(0);
-	}
-	//Shared memory creation and putting log fd info
-	int shmid1;
-	key_t key1;
-	key1=300;
-	int size=10000;
-	struct logfds *shm;
-	if ((shmid1 = shmget (key1, size, IPC_CREAT | 0666)) == -1) {
-	   perror("shmget: shmget failed"); exit(1); } else {
-	   (void) fprintf(stderr, "shmget: shmget returned %d\n", shmid1);
-	}
-	shm = (struct logfds *)shmat(shmid1, NULL, 0);
-	shm->number=logs.number;
-	for(i=0;i<logs.number;i++)
-		shm->fd[i]=logs.fd[i];
-	//Creating polling fds
+	pipe(logs.pipefd);
+	logs.number=3;
 	struct pollfd fds[10];
 	for(i=0;i<logs.number;i++){
 		fds[i].fd=logs.fd[i];
 		fds[i].events=POLLRDNORM;
 	}
-	int temp;
-	int j=0;
-	int k;
-	printf("\nlog fds are created................");
-	printf("\nServer started..................");
-	while(1){
-		int retstatus=poll(fds,logs.number,2000);
-		// printf("Polling\n");
-		if(retstatus > 0){
-			// printf("Got message\n");
-			for(j=0;j<logs.number;j++)
-			{
-				if(fds[j].revents == fds[j].events){
-					char buf[100];
-					printf("Logged: ");
-					if(read(fds[j].fd,buf,100) > 0)
-					{
-						printf("%s\n",buf);
-						if(strcasecmp(buf,"s1\n") == 0 && n_s1 > 0)
+	fds[logs.number].fd=logs.pipefd[0];
+	fds[logs.number].events=POLLRDNORM;
+	int pid;
+	if((pid = fork())<0)
+		printf("Error for fork");
+	else if(pid > 0)
+	{
+		int temp;
+		int j=0;
+		int k;
+		printf("\nClient fds are created................");
+		printf("\nServer started..................");
+		while(1){
+			int retstatus=poll(fds,logs.number+1,2000);
+			// printf("Polling\n");
+			if(retstatus > 0){
+				// printf("Got message\n");
+				for(j=0;j<logs.number+1;j++)
+				{
+					if(fds[j].revents == fds[j].events){
+						char buf[100];
+						printf("Logged: ");
+						if(read(fds[j].fd,buf,100) > 0)
 						{
-							n_s1--;
-							printf("S1 allocated\n");
-							write(logwrite.fd[j],"S1fifo",35);
+							printf("\n%s\n",buf);
 						}
-						else if(strcasecmp(buf,"s2\n") == 0 && n_s2 > 0)
-						{
-							n_s2--;
-							printf("S2 allocated\n");
-							write(logwrite.fd[j],"S2fifo",35);
-						}
-						else{
-							printf("No servers avaliable\n");
-						}
+						fflush(stdout);
 					}
-					fflush(stdout);
 				}
 			}
+			fflush(stdout); 
 		}
-		fflush(stdout); 
+	}
+	else{
+			char message[]="This is from pipe";
+			write(logs.pipefd[1],message,sizeof(message));
+			signal(SIGUSR1,sig_usr);
+    		raise(SIGUSR1);
 	}
 	return 0;
+}
+
+
+static void sig_usr(int signo)
+{
+	if (signo == SIGUSR1){
+		printf("\nreceived SIGUSR1\n");
+		int fd=open("mainfifo2",O_RDWR);
+		char message[]="got this from signal";
+		write(fd,message,sizeof(message));	
+	}
 }
