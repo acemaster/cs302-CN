655f9f18ecac310cf9a3f17289f43c483a51a1e5
acemaster <vivekhtc25@gmail.com>
2016-02-15 23:14:58 +0530

    Started sockets

---
+++ sockets/simplesocket/server.c
@@ -0,0 +1,48 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include "vivek.h"
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sfd,nsfd,n,portno;
+     socklen_t clientn;
+     struct sockaddr_in myaddr,clientaddr;
+     portno=atoi(argv[1]);
+     sfd=socket(AF_INET,SOCK_STREAM,0);
+     if(sfd<0)
+        error("Socket not initilizated");
+    bzero((char *)&myaddr,sizeof(myaddr));
+    myaddr.sin_family=AF_INET;
+    myaddr.sin_addr.s_addr=INADDR_ANY;
+    myaddr.sin_port=htons(portno);
+    if((bind(sfd,(struct sockaddr *) &myaddr, sizeof(myaddr)))<0)
+        error("Error on binding: ");
+    listen(sfd,5);
+    clientn=sizeof(clientaddr);
+    nsfd=accept(sfd,(struct sockaddr *) &clientaddr, &clientn);
+    if(nsfd < 0)
+        error("Error on getting client: ");
+    char buffer[250];
+    bzero(buffer,250);
+    while(1)
+    {
+        bzero(buffer,250);
+        n=read(nsfd,buffer,100);
+        if(n > 0)
+        {
+            if(strcmp(buffer,"exit\n") == 0)
+                break;
+            printf("Client: %s\n",buffer);
+            buffer[0]=toupper(buffer[0]);
+            n=write(nsfd,buffer,sizeof(buffer));
+        }
+    }
+    close(nsfd);
+    close(sfd);
+}
