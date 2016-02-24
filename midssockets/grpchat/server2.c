7bb7a35ba919e9e9b40366d438c47df0ec939b11
acemaster <vivekhtc25@gmail.com>
2016-02-24 22:34:14 +0530

    Group chat almost complete

---
+++ midssockets/grpchat/server2.c
@@ -0,0 +1,123 @@
+#include "vivek.h"
+
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <sys/select.h>
+
+pthread_t clientthread[10];
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+struct clientthreadarg{
+	int nsfd;
+    char groupname[100];
+    int clientid;
+};
+
+int clientno=0;
+int clients[10];
+void *clientthreadfunc(void *arg)
+{
+	char buffer[250];
+	struct clientthreadarg *argv=arg;
+	int nsfd=argv->nsfd;
+	int n;
+    int i;
+	while(1){
+        bzero(buffer,250);
+        n=read(nsfd,buffer,100);
+        if(n > 0)
+        {
+            if(strcmp(buffer,"exit\n") == 0)
+            {
+                close(nsfd);
+                break;
+            }
+            printf("Client %s: %s\n",argv->groupname,buffer);
+            buffer[0]=toupper(buffer[0]);
+            n=write(nsfd,buffer,sizeof(buffer));
+            for(i=0;i<clientno;i++)
+            {
+                if(i!=argv->clientid);
+                    n=write(clients[i],buffer,sizeof(buffer));
+            }
+        }
+    }
+    printf("Client leaving...........\n");
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+     printf("%s\n",argv[2]);
+     int sfd[20],nsfd,n,portno[20];
+     fd_set readset;
+     FD_ZERO(&readset);
+     struct timeval timeptr;
+     timeptr.tv_sec=0;
+     timeptr.tv_usec=0;
+     int max=0;
+     //==========End Select config===============
+     socklen_t clientn;
+     struct sockaddr_in myaddr,clientaddr;
+     int i=0;
+     for(i=0;i<argc-1;i++)
+    {
+        portno[i]=atoi(argv[i+1]);
+        sfd[i]=socket(AF_INET,SOCK_STREAM,0);
+         if(sfd[i]<0)
+            error("Socket not initilizated");
+        bzero((char *)&myaddr,sizeof(myaddr));
+        myaddr.sin_family=AF_INET;
+        myaddr.sin_addr.s_addr=INADDR_ANY;
+        myaddr.sin_port=htons(portno[i]);
+        if((bind(sfd[i],(struct sockaddr *) &myaddr, sizeof(myaddr)))<0)
+            error("Error on binding: ");
+        listen(sfd[i],5);
+    }
+    for(i=0;i<argc-1;i++)
+    {
+        FD_SET(sfd[i],&readset);
+        if(sfd[i] > max)
+            max=sfd[i];
+    }
+    int service=0;
+    printf("\nStarted mini server................");
+    printf("\nMini server running..................");
+    clientn=sizeof(clientaddr);
+    char buffer[250];
+    bzero(buffer,250);
+    int pid=1;
+    while(1 && pid > 0)
+    {
+        int retstatus=select(max+1,&readset,NULL,NULL,&timeptr);
+        if(retstatus > 0)
+        {
+            // printf("Got message\n");
+            for(i=0;i<argc-1;i++)
+            {
+                if(FD_ISSET(sfd[i],&readset))
+                {
+                    nsfd=accept(sfd[i],(struct sockaddr *) &clientaddr, &clientn);
+                    struct clientthreadarg temp;
+                    temp.nsfd=nsfd;
+                    strcpy(temp.groupname,argv[2]);
+                    temp.clientid=clientno;
+                    clients[clientno]=nsfd;
+                    pthread_create(&clientthread[clientno++],NULL,clientthreadfunc,&temp);
+                }
+            }
+        }
+        fflush(stdout); 
+        for(i=0;i<argc-1;i++)
+        {
+            FD_SET(sfd[i],&readset);
+            if(sfd[i] > max)
+                max=sfd[i];
+        }
+    }
+}
